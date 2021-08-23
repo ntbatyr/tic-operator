a854b098c11042bb60072e6e0dfb6d537b8c1245
Batyr <ntbatyr@mail.ru>
2021-08-23 15:39:33 +0300

    changes

--- src/database/migrations/create_invest_programs_table.php
+++ src/database/migrations/create_invest_programs_table.php
@@ -3,10 +3,17 @@
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Schema\Blueprint;
 
+Capsule::schema()->dropIfExists('invest_programs');
+
 Capsule::schema()->table('invest_programs', function (Blueprint $table) {
-    $table->increments('id');
-    $table->string('name');
-    $table->decimal('min_deposit', 12, 6);
-    $table->float('annual_percent');
-    $table->boolean('active')->default(0);
+    try {
+        $table->increments('id');
+        $table->string('name');
+        $table->decimal('min_deposit', 12, 6);
+        $table->float('annual_percent');
+        $table->boolean('active')->default(0);
+    } catch (Exception $exception) {
+        echo "Error: {$exception->getMessage()} \n";
+        echo "Trace: {$exception->getTraceAsString()} \n";
+    }
 });
--- src/database/migrations/create_user_invest_programs_table.php
+++ src/database/migrations/create_user_invest_programs_table.php
@@ -3,10 +3,17 @@
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Schema\Blueprint;
 
+Capsule::schema()->dropIfExists('user_invest_programs');
+
 Capsule::schema()->table('user_invest_programs', function (Blueprint $table) {
-    $table->increments('id');
-    $table->bigInteger('user_id');
-    $table->bigInteger('invest_program_id');
-    $table->decimal('amount', 12, 6);
-    $table->unique(['user_id', 'invest_program_id']);
+    try {
+        $table->increments('id');
+        $table->bigInteger('user_id');
+        $table->bigInteger('invest_program_id');
+        $table->decimal('amount', 12, 6);
+        $table->unique(['user_id', 'invest_program_id']);
+    } catch (Exception $exception) {
+        echo "Error: {$exception->getMessage()} \n";
+        echo "Trace: {$exception->getTraceAsString()} \n";
+    }
 });
